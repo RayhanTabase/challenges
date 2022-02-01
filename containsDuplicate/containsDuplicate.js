b8f13c9f96326c6157e93a50e44cb98f7e052c57
Salim Abdulai <maouslayer@gmail.com>
2022-02-01 07:28:06 +0000

    Add leetcode challenge

---
+++ containsDuplicate/containsDuplicate.js
@@ -0,0 +1,28 @@
+/**
+  Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+  Example 1:
+
+  Input: nums = [1,2,3,1]
+  Output: true
+  Example 2:
+
+  Input: nums = [1,2,3,4]
+  Output: false
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+var containsDuplicate = function(nums) {
+    let dict = {};
+    let isDist = false;
+    nums.forEach((num) => {
+        if(dict[num]) { 
+            isDist = true;
+        };
+        dict[num] = true;  
+    })
+    return isDist;
+};
