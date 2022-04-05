94fede3af5f34cba3a574034e42b87b8cc559019
Salim Abdulai <maouslayer@gmail.com>
2022-04-05 23:30:33 +0000

    Update simpleTraversal.py

--- BFS/simpleTraversal.py
+++ BFS/simpleTraversal.py
@@ -1,4 +1,5 @@
 /*
+
 link : https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
 The height of a binary tree is the number of edges between the tree's root and its furthest leaf. For example, the following binary tree is of height :
 
